a3621f043c5a68edd4573746eb7df42b24e6f944
faker007 <rlaqjadud195@naver.com>
2022-03-03 10:09:33 +0900

    2022-03-03 nextjs learning - 1

---
+++ pages/user-profile.js
@@ -0,0 +1,11 @@
+const UserProfilePage = (props) => {
+  return <h1>{props.username}</h1>;
+};
+
+export default UserProfilePage;
+
+export async function getServerSideProps(context) {
+  const { params, req, res } = context;
+
+  return { props: { username: 'GyeJin' } };
+}
